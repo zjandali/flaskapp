1e61f188890c3e763efaa879cd0343272288ab1e
Zain Jandali <yzjandali@berkeley.edu>
2021-03-25 12:03:32 -0400

    setup

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/zain')
+def zain():
+    return render_template("minimal-mistakes/index.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(debug=True)
